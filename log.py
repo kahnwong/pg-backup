85b174a280467eb718961108e5b881dac27ef697
kahnwong <karn.wong@protonmail.ch>
2021-01-19 18:26:15 +0700

    init commit

---
+++ log.py
@@ -0,0 +1,9 @@
+class Logger:
+    def info(self, text: str):
+        print("[INFO] " + text)
+
+    def warn(self, text: str):
+        print("[WARN] " + text)
+
+    def error(self, text: str):
+        print("[ERROR] " + text)
